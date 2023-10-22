74319173cd330a2cb3d7be82a0f8075c98cb2103
Cihat Köse <cihat.kose@hotmail.com>
2023-10-22 22:05:55 +0200

    Update 10.04.2023.sql

--- 10.04.2023.sql
+++ 10.04.2023.sql
@@ -2,7 +2,6 @@
 
 -- Soru :  isim ve soyisimlerin ilk karakterden sonraki kısımlarına * atarak listeletiniz.
 -- İsmet Temur  ->  İ****  T****
-
 select
 first_name,last_name,
 concat( 
@@ -11,18 +10,14 @@ left(last_name,1),   repeat('*', length(last_name)-1)
 ) as gizliAdSoyad
 from customer;
 
-
 -- Soru : isim ve soyisimi örneğin Ismet Temur -> IT TR şeklinde bitişik yazdırınız.
-
 select
 first_name,last_name,
 concat( 
 left(first_name,1), right(first_name,1)," ",left(last_name,1), right(last_name,1)) as KısaAdSoyad
 from customer;
 
-
 -- Soru : isim ve soyisimi örneğin Ismet Temur -> I***T T***R şeklinde bitişik yazdırınız
-
 select
 first_name,last_name,
 concat( 
