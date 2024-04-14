f3af91b2f412a1aa5c6e41f954ab91d6b9cb35b3
Cihat Köse <cihat.kose@hotmail.com>
2024-04-14 14:20:21 +0200

    Update MySQLCurrentLesson01.sql

--- MySQL/MySQLCurrentLesson01.sql
+++ MySQL/MySQLCurrentLesson01.sql
@@ -11,16 +11,24 @@ use sakila;
 use world; -- Şu anda bulunduğum kısım
 
 select * from country; -- Country'deki bütün bilgileri seçip getir
-select Code,Name from country; -- İstenen alanları da alabilirim, isimlerini yazmam yeterli
+select code,name from country; -- İstenen alanları da alabilirim, isimlerini yazmam yeterli
 select * from city;
-select Name,population from city; -- City'den name ve populationu getir.
+select name,population from city; -- city'den name ve populationu getir.
+select NAME,POPULATION from city; -- kolon adları büyük,küçük yazılabilir,
+                                   -- tablo adları aynen yazılmalıdır.
 select count(id) from city; -- Kaç tane id var
 select count(name) from city; -- Kaç tane name var
 select count(*) from city; -- Kaç tane satır var
 select count(*) as SehirSayisi from city; -- as ilgili veriye uygun başlık vermek için kullanılabilir
-select Code as Kod, Name as Ad from country; -- SQL : db sorgulama dili
+select code as kod, name as ad from country; -- SQL : db sorgulama dili
+
 select distinct(name) from city; -- Benzersiz ismi olan şehirler
 select count(distinct(name)) from city; -- Benzersiz ismi olan şehirlerin sayısı
+
+select name as ad, population as Nüfus from city; -- as ilgili veriye uygun başlık vermek için kullanılabilir.
+select * from city;
+select name from city;
+select distinct(name) from city; -- Benzersiz ismi olan şehirlerin listesi
 
 select 12-5; -- Selecti hesap makinesi gibi kullanabiliriz
 select 45/6*4;
